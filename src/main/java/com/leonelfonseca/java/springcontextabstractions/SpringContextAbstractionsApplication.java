4030eeb2e72f5dbd4d124341f0a1ef4c83aff2f8
leonelmfonseca <leonelfonseca@gmail.com>
2024-12-16 16:25:01 +0000

    chore: init commit

---
+++ src/main/java/com/leonelfonseca/java/springcontextabstractions/SpringContextAbstractionsApplication.java
@@ -0,0 +1,17 @@
+package com.leonelfonseca.java.springcontextabstractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContextAbstractionsApplication {
+
+
+	/**
+	 *  this is a comment
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContextAbstractionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonelfonseca/java/springcontextabstractions/SpringContextAbstractionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonelfonseca.java.springcontextabstractions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringContextAbstractionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
