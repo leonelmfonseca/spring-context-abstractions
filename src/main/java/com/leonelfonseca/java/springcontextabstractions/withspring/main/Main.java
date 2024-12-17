becc1e82bb48224c9a7e41acd625ee1d1bfe4497
leonelmfonseca <leonelfonseca@gmail.com>
2024-12-17 12:06:39 +0000

    refactor: add main class to "using abstractions with spring"

--- src/main/java/com/leonelfonseca/java/springcontextabstractions/withspring/main/Main.java
+++ src/main/java/com/leonelfonseca/java/springcontextabstractions/withspring/main/Main.java
@@ -1,19 +1,48 @@
 package com.leonelfonseca.java.springcontextabstractions.withspring.main;
 
 import com.leonelfonseca.java.springcontextabstractions.withspring.model.Comment;
+import com.leonelfonseca.java.springcontextabstractions.withspring.proxies.EmailCommentNotificationProxy;
+import com.leonelfonseca.java.springcontextabstractions.withspring.repositories.DBCommentRepository;
 import com.leonelfonseca.java.springcontextabstractions.withspring.services.CommentService;
-import com.leonelfonseca.java.springcontextabstractions.withspring.config.AbstractionsConfiguration;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-  public static void main(String[] args) {
-    var context = new AnnotationConfigApplicationContext(AbstractionsConfiguration.class);
 
+  public static void main(String[] args) {
+
+    var comentRepository = new DBCommentRepository();
+
+    var commentCommentNotificationProxy =
+        new EmailCommentNotificationProxy(); // Creates the instance for the dependencies
+
+    // Creates the instance of the service class and providing the dependencies
+    var commentService = new CommentService(comentRepository, commentCommentNotificationProxy);
+
+    // Creates an instance of comment to send as a parameter to the publish comment use case
     var comment = new Comment();
     comment.setAuthor("Bugs Bunny");
     comment.setText("That's all folks!");
 
-    var commentService = context.getBean(CommentService.class);
-    commentService.publishComment(comment);
+    commentService.publishComment(comment); // Calls the publish comment use case
   }
+
+  /**
+   * When/if you want to change repos or proxies, just create a new class, implementing the desired
+   * interface and override correspondent methods. Then, you just have to create the objects that implement
+   * desired functionality, and pass it to service.
+   *
+   *
+   *   ├── main
+   *   │      └── Main.java
+   *   ├── model
+   *   │      └── Comment.java
+   *   ├── proxies
+   *   │      ├── CommentNotificationProxy.java
+   *   │      └── EmailCommentNotificationProxy.java
+   *   ├── repositories
+   *   │      ├── CommentRepository.java
+   *   │      └── DBCommentRepository.java
+   *   └── services
+   *          └── CommentService.java
+   */
+
 }
